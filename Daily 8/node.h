c9180ddcde036c1679d2e04f4564e29f9ba457b2
aallman1 <102977324+aallman1@users.noreply.github.com>
2023-01-21 16:40:44 -0500

    Add files via upload

---
+++ Daily 8/node.h
@@ -0,0 +1,17 @@
+#ifndef NODE_H
+#define NODE_H
+
+struct node
+{
+	int priority_level;
+	int data_item;
+};
+
+typedef struct node Node;
+void node_destroy(Node** pNode);
+void heapify_nodes(Node** data, int size);
+void fix_down_nodes(Node** data, int index, int number_of_elements);
+void remove_max_node(Node** data, int* size);
+
+
+#endif // !NODE_H
